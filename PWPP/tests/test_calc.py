adec3f548d8a1a08835d7b2b4770bb43825b8fd2
Tyler Wixtrom <tjwixtrom@gmail.com>
2018-04-19 12:14:32 -0500

    Created folder and file for testing

---
+++ PWPP/tests/test_calc.py
@@ -0,0 +1,3 @@
+from ..calc import (get_isobaric_variables, get_precip, get_timestep_precip,
+                   get_temp_2m, get_q_2m, get_u_10m, get_v_10m, get_mslp, get_uh,
+                   get_cape, get_dbz)
